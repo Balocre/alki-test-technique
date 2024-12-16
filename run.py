a236852f05ba0a25ed26a250d8fb75aacd0732b8
Antoine Audras <antoineaudras@gmail.com>
2024-12-16 16:35:10 +0100

    feat(run): add logic for calling test from the run script and add some config

--- run.py
+++ run.py
@@ -2,11 +2,12 @@ import os
 
 import hydra
 from darts import TimeSeries
+from darts.utils.model_selection import train_test_split
 from hydra.utils import instantiate
 from omegaconf import DictConfig
 
 from src.data import build_piped_flux_filter, get_df_from_influxdb
-from src.train import fit
+from src.train import eval, fit
 
 CLIENT_HOST = os.getenv("INFLUXDB_V2_URL")
 CLIENT_ORG = os.getenv("INFLUXDB_V2_ORG")
@@ -15,7 +16,7 @@ CLIENT_TOKEN = os.getenv("INFLUXDB_V2_TOKEN")
 BUCKET = "technical-test"
 
 
-def train(cfg: DictConfig):
+def _load_model_from_cfg(cfg: DictConfig):
     model = instantiate(cfg.model)
 
     # load model from checkpoint for finetuning
@@ -28,16 +29,28 @@ def train(cfg: DictConfig):
         except ValueError:
             print("Using untrained model")
 
-    flux_query_filters = list()
+    return model
+
+
+def _build_flux_filter_from_cfg(cfg: DictConfig):
+    flux_filters = list()
     for key, values in cfg.data.filters.builder_arguments.items():
-        flux_query_filters.append(build_piped_flux_filter(key, values))
+        flux_filters.append(build_piped_flux_filter(key, values))
+
+    return flux_filters
+
+
+def train(cfg: DictConfig):
+    model = _load_model_from_cfg(cfg)
+
+    flux_filters = _build_flux_filter_from_cfg(cfg)
 
     data_df = get_df_from_influxdb(
         bucket=BUCKET,
         influxdb_host=CLIENT_HOST,
         influxdb_org=CLIENT_ORG,
         influxdb_token=CLIENT_TOKEN,
-        flux_query_filters=flux_query_filters,
+        flux_query_filters=flux_filters,
         start="-8y",
         stop="now()",
     )
@@ -53,13 +66,54 @@ def train(cfg: DictConfig):
     fit(model, series, epochs=cfg.train_parameters.epochs)
 
 
-def eval():
-    pass
+def test(cfg):
+    model = _load_model_from_cfg(cfg)
+
+    flux_filters = _build_flux_filter_from_cfg(cfg)
+
+    data_df = get_df_from_influxdb(
+        bucket=BUCKET,
+        influxdb_host=CLIENT_HOST,
+        influxdb_org=CLIENT_ORG,
+        influxdb_token=CLIENT_TOKEN,
+        flux_query_filters=flux_filters,
+        start="-8y",
+        stop="now()",
+    )
+
+    series = TimeSeries.from_group_dataframe(
+        data_df,
+        group_cols="CUSTOMER",
+        value_cols="QUANTITY",
+        fill_missing_dates=True,
+        freq="D",
+    )
+
+    input_size = model.model_params.get("input_chunk_length", None)
+    horizon = model.model_params.get("output_chunk_length", None)
+
+    series, series_val = train_test_split(
+        series,
+        test_size=cfg.test_parameters.test_size,
+        axis=1,
+        input_size=input_size,
+        horizon=horizon,
+        vertical_split_type="model-aware",
+    )
+
+    n = cfg.test_parameters.n
+    num_samples = cfg.test_parameters.num_samples
+    eval(model, series, series_val, n, num_samples)
 
 
-@hydra.main(config_path="./conf/", config_name="train", version_base="1.3")
+@hydra.main(config_path="./conf/", config_name="run", version_base="1.3")
 def main(cfg: DictConfig):
-    train(cfg)
+    if cfg.mode == "train":
+        print("Launching train")
+        train(cfg)
+    else:
+        print("Testing model")
+        test(cfg)
 
 
 if __name__ == "__main__":
